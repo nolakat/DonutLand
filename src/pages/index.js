e2c031a0d538fe8733dfbac50edb1eaf663aa4c9
Kate Nolan <katenolan@Kates-MBP.fios-router.home>
2020-04-07 14:44:13 -0400

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
+import { Canvas } from 'react-three-fiber'
+import { useSpring, a } from 'react-spring/three'
 
-export default () => <div>Hello world!</div>
+import './style.css'
+
+const Box = () => {
+  const [hovered, setHovered ] = useState(false)
+  const [active, setActive] = useState(false)
+  const props = useSpring({
+   scale: active ? [1.5, 1.5, 1.5]: [1, 1, 1],
+   color: hovered ? "hotpink" : "gray"
+  })
+
+  return(
+    <a.mesh 
+      onPointerOver={()=> setHovered(true)} 
+      onPointerOut={()=> setHovered(false)}
+      onClick={()=> setActive(!active)}
+      scale={props.scale}
+      >
+      <boxBufferGeometry 
+        attach="geometry"
+        args={[1, 1, 1]}
+      />
+      <a.meshBasicMaterial
+        attach="material"
+        color={props.color}
+      />
+    </a.mesh>
+  )
+}
+
+
+export default () => (  
+  <Canvas>
+    <Box />
+  </Canvas>
+)
