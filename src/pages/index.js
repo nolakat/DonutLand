207a4457e4ca6052117202cf4a0409818d8af95e
Kate Nolan <katenolan@Kates-MBP.fios-router.home>
2020-04-07 15:13:45 -0400

    useFrame instead of useRender

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,26 @@
-import React, { useState } from "react"
-import { Canvas } from 'react-three-fiber'
+import React, { useState, useRef } from "react"
+import { Canvas, useFrame } from 'react-three-fiber'
 import { useSpring, a } from 'react-spring/three'
 
 import './style.css'
 
 const Box = () => {
+  const meshRef = useRef();
   const [hovered, setHovered ] = useState(false)
   const [active, setActive] = useState(false)
   const props = useSpring({
    scale: active ? [1.5, 1.5, 1.5]: [1, 1, 1],
    color: hovered ? "hotpink" : "gray"
   })
+  
+
+  useFrame(() => {
+    meshRef.current.rotation.y += 0.01
+  })
 
   return(
-    <a.mesh 
+    <a.mesh
+      ref={meshRef} 
       onPointerOver={()=> setHovered(true)} 
       onPointerOut={()=> setHovered(false)}
       onClick={()=> setActive(!active)}
